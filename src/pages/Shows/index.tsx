197b7b62bff23a77831196581b336e7556993e28
ndrdg <96367205+n-d-r-d-g@users.noreply.github.com>
2024-09-13 10:08:19 +0400

    Fixed new show order + added season & episode

--- src/pages/Shows/index.tsx
+++ src/pages/Shows/index.tsx
@@ -3,13 +3,13 @@ import Cookies from 'js-cookie';
 import { observer } from 'mobx-react-lite';
 import { FormEvent, useCallback, useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { IApiShow } from '../../@types/auth';
 import { useAuth } from '../../hooks/useAuth';
 import showStore, { IShow } from '../../store/show';
-import { IApiShow } from '../../@types/auth';
 import ShowList from './components/ShowList';
 
 const Shows = observer(function () {
-  const { user, isSignedIn, signIn, signOut } = useAuth();
+  const { user, signOut } = useAuth();
   const [show, setShow] = useState<IShow>({
     order: -1,
     title: '',
@@ -38,77 +38,57 @@ const Shows = observer(function () {
     });
   }, [user]);
 
-  const handleSignInFormSubmit = useCallback(
-    (e: FormEvent<HTMLFormElement>) => {
-      e?.preventDefault?.();
-      const formTarget = e.target as HTMLFormElement;
-      const username = (
-        formTarget.elements.namedItem('username') as HTMLInputElement
-      ).value;
-      const password = (
-        formTarget.elements.namedItem('password') as HTMLInputElement
-      ).value;
+  const handleSignOut = useCallback(() => {
+    signOut();
+    showStore.setShows([]);
+  }, [signOut]);
 
-      signIn({ username, password });
+  const handleAddShow = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      const showId = uuidv4();
+      const showOrder = showStore.shows[showStore.shows.length - 1].order + 1;
+
+      showStore.addShow({
+        ...show,
+        id: showId,
+        order: showOrder,
+      });
+
+      axios.post(`${import.meta.env.VITE_APP_API_URL}shows`, {
+        id: showId,
+        name: show.title,
+        url: show.url,
+        season: show.season,
+        episode: show.episode,
+        image: show.imgUrl,
+      } as IApiShow);
+
+      setShow({
+        id: '',
+        order: -1,
+        title: '',
+        url: '',
+        season: 1,
+        episode: 1,
+      });
     },
-    [signIn]
+    [show]
   );
 
   return (
     <>
-      {isSignedIn && user?.username && <h1>Welcome {user.username}</h1>}
-      {!isSignedIn && (
-        <form id="signin" onSubmit={handleSignInFormSubmit}>
-          <input name="username" placeholder="Username" />
-          <input name="password" type="password" placeholder="Password" />
-          <button type="submit">Login</button>
-        </form>
-      )}
-      {isSignedIn && (
-        <button
-          type="button"
-          onClick={() => {
-            signOut();
-            showStore.setShows([]);
-          }}
-          disabled={!user}
-        >
-          Logout
-        </button>
-      )}
+      {user?.username && <h1>Welcome {user.username}</h1>}
+      <button type="button" onClick={handleSignOut} disabled={!user}>
+        Log out
+      </button>
       <form
-        onSubmit={(e) => {
-          e.preventDefault();
-
-          const showId = uuidv4();
-          const showOrder = showStore.shows.length;
-
-          showStore.addShow({
-            ...show,
-            id: showId,
-            order: showOrder,
-          });
-
-          axios.post(`${import.meta.env.VITE_APP_API_URL}shows`, {
-            id: showId,
-            name: show.title,
-            url: show.url,
-            season: show.season,
-            episode: show.episode,
-            image: show.imgUrl,
-          } as IApiShow);
-
-          setShow({
-            id: '',
-            order: -1,
-            title: '',
-            url: '',
-            season: 1,
-            episode: 1,
-          });
-        }}
+        onSubmit={handleAddShow}
+        className="flex flex-row justify-center items-center gap-2"
       >
         <input
+          name="title"
           placeholder="Title"
           value={show.title}
           onChange={(e) =>
@@ -116,10 +96,33 @@ const Shows = observer(function () {
           }
         />
         <input
+          name="url"
           placeholder="Url"
           value={show.url}
           onChange={(e) =>
             setShow((prev) => ({ ...prev, url: e.target.value }))
+          }
+        />
+        <input
+          name="season"
+          type="number"
+          placeholder="Season"
+          defaultValue={show.season}
+          min={0}
+          max={99}
+          onChange={(e) =>
+            setShow((prev) => ({ ...prev, season: +e.target.value }))
+          }
+        />
+        <input
+          name="episode"
+          type="number"
+          placeholder="Episode"
+          defaultValue={show.episode}
+          min={0}
+          max={99}
+          onChange={(e) =>
+            setShow((prev) => ({ ...prev, episode: +e.target.value }))
           }
         />
         <button type="submit">Add show</button>
