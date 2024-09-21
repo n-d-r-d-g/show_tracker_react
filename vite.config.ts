e48fd82a6a2157c00161f2243c2fd7821dfa0e86
ndrdg <96367205+n-d-r-d-g@users.noreply.github.com>
2024-09-21 21:47:11 +0400

    Added shadcn + button component

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
