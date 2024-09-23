ee824df5ce317d61faa83a01f3989d23e7307d54
ndrdg <96367205+n-d-r-d-g@users.noreply.github.com>
2024-09-23 11:03:37 +0400

    Modified app logo styling

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,7 +1,9 @@
 import { PropsWithChildren } from 'react';
-import { ThemeSwitch } from '../../components/theme-switch';
-import { cn } from '../../lib/utils';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import { ThemeSwitch } from '../../components/theme-switch';
+import { Button } from '../../components/ui/button';
+import { cn } from '../../lib/utils';
 
 type Props = PropsWithChildren<{
   className?: string;
@@ -12,8 +14,12 @@ export default function DefaultLayout({ className, children }: Props) {
 
   return (
     <>
-      <header className="w-full max-w-full sticky top-0 flex flex-row justify-between items-center px-[max(calc((100%-75rem)/2),0.5rem)] py-2 bg-neutral-50 dark:bg-neutral-900/50 border-b-2 border-neutral-200/50 dark:border-neutral-800/50">
-        <p className="uppercase text-sm font-bold">{tCommon('title')}</p>
+      <header className="w-full max-w-full sticky top-0 flex flex-row justify-between items-center px-[max(calc((100%-75rem)/2),0.25rem)] py-1 bg-neutral-50 dark:bg-neutral-900/50 border-b-2 border-neutral-200/50 dark:border-neutral-800/50">
+        <Button variant="link" className="h-auto px-1 py-1" asChild>
+          <Link to="/" className="uppercase font-mono text-sm font-bold">
+            {tCommon('title')}
+          </Link>
+        </Button>
         <ThemeSwitch />
       </header>
       <main
