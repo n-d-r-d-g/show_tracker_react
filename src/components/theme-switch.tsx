f1b6b9e033abe83afb37ee06f265fa8cc6939dd3
ndrdg <96367205+n-d-r-d-g@users.noreply.github.com>
2024-09-23 11:42:35 +0400

    Refactored theme switch

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -1,6 +1,5 @@
-import { Monitor, Moon, Sun } from 'lucide-react';
-import { useRef } from 'react';
 import { useTranslation } from 'react-i18next';
+import { THEMES } from '../constants';
 import { useTheme } from './theme-provider';
 import { Button } from './ui/button';
 import {
@@ -10,43 +9,56 @@ import {
   DropdownMenuTrigger,
 } from './ui/dropdown-menu';
 
-export function ThemeSwitch() {
-  const { theme, setTheme } = useTheme();
-  const { t: tCommon } = useTranslation('common');
-  const themes = useRef({
-    system: {
-      Icon: Monitor,
-      labelKey: 'system',
-    },
-    light: {
-      Icon: Sun,
-      labelKey: 'light',
-    },
-    dark: {
-      Icon: Moon,
-      labelKey: 'dark',
-    },
-  } as const);
+function ThemeIcons() {
+  const { theme } = useTheme();
 
-  const ActiveIcon = themes.current[theme].Icon;
+  return THEMES.map((themeObj) => {
+    const Icon = themeObj.Icon;
+
+    return (
+      <Icon
+        key={themeObj.key}
+        data-active={themeObj.key === theme}
+        className="absolute h-full w-full p-2 text-neutral-500 dark:text-neutral-300 -rotate-90 scale-0 opacity-0 data-[active='true']:rotate-0 data-[active='true']:scale-100 data-[active='true']:opacity-100 transition-all"
+      />
+    );
+  });
+}
+
+function ThemeDropdownMenuItems() {
+  const { setTheme } = useTheme();
+  const { t: tCommon } = useTranslation('common');
+
+  return THEMES.map((themeObj) => (
+    <DropdownMenuItem
+      key={themeObj.key}
+      onClick={() => {
+        setTheme(themeObj.key);
+      }}
+    >
+      {tCommon(`theme.${themeObj.labelKey}`)}
+    </DropdownMenuItem>
+  ));
+}
+
+export function ThemeSwitch() {
+  const { t: tCommon } = useTranslation('common');
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" title={tCommon('theme.toggle')}>
-          <ActiveIcon className="h-[1.2rem] w-[1.2rem]" />
+        <Button
+          variant="ghost"
+          size="icon"
+          title={tCommon('theme.toggle')}
+          className="relative h-9 w-9"
+        >
+          <ThemeIcons />
           <span className="sr-only">{tCommon('theme.toggle')}</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        {Object.entries(themes.current).map(([themeName]) => (
-          <DropdownMenuItem
-            key={themeName}
-            onClick={() => setTheme(themeName as keyof typeof themes.current)}
-          >
-            {tCommon(`theme.${themeName as keyof typeof themes.current}`)}
-          </DropdownMenuItem>
-        ))}
+        <ThemeDropdownMenuItems />
       </DropdownMenuContent>
     </DropdownMenu>
   );
--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,20 @@
+import { Monitor, Moon, Sun } from 'lucide-react';
+
 export const DEBOUNCE_TIMEOUT_IN_MS = 300;
+export const THEMES = [
+  {
+    key: 'system',
+    Icon: Monitor,
+    labelKey: 'system',
+  },
+  {
+    key: 'light',
+    Icon: Sun,
+    labelKey: 'light',
+  },
+  {
+    key: 'dark',
+    Icon: Moon,
+    labelKey: 'dark',
+  },
+] as const;
