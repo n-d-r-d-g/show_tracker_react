28784c67aa181be942a16d5e33607ad8ba5dbc83
ndrdg <96367205+n-d-r-d-g@users.noreply.github.com>
2024-09-23 12:52:27 +0400

    Updated system theme icon

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,10 @@
-import { Monitor, Moon, Sun } from 'lucide-react';
+import { Moon, Sun, SunMoon } from 'lucide-react';
 
 export const DEBOUNCE_TIMEOUT_IN_MS = 300;
 export const THEMES = [
   {
     key: 'system',
-    Icon: Monitor,
+    Icon: SunMoon,
     labelKey: 'system',
   },
   {
